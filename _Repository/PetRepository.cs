9d32a800217edf19be5e669a9aa3a181bbd4c02f
ejakit <umarezaq@gmail.com>
2026-02-13 20:40:53 +0700

    lanjut besok mw tidur

---
+++ _Repository/PetRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+using petManagement.Models;
+
+namespace petManagement._Repository
+{
+    public class PetRepository : BaseRepository, IPetRepository
+    {
+        public void Add(petModel petmodel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Edit(petModel petModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<petModel> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<petModel> GetByValue(string value)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
